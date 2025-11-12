2ef631b2190f8bc843d15b34fa8733c5e6b92b4b
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-12 19:30:21 +0545

    Task 3 completed

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Task_3
+﻿using System;
+
+namespace Task_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Ask user to input age
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Using if-else-if
+            if (age < 13)
+            {
+                Console.WriteLine("You are a child.");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else // age >= 20
+            {
+                Console.WriteLine("You are an adult.");
+            }
+
+            Console.ReadLine(); // Keep console open
         }
     }
 }
